61e4e8b8300d806a69687046e7fe744ee3744ebc
Bartosz Malinowski <bartek4c@outlook.com>
2017-08-26 21:21:08 +0100

    Geaolocation API code added

--- html/Geolocation/code.js
+++ html/Geolocation/code.js
@@ -1,17 +1,50 @@
-var hasVideo = !!(document.createElement('video').canPlayType("mp4"));
+function loadDemo() {
+    if(navigator.geolocation) {
+        console.log("Geolocation supported.");
+        /*
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback,
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 10000
+            }
+        );
+        */
 
-var vid = document.getElementById("myVideo");
+        var watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
 
+        navigator.geolocation.clearWatch(watchId);
+    } else {
+        console.log("Geolocation is not supported in your browser.");
+    }
+}
 
-document.getElementById("play").addEventListener("click", function () {
-    console.log(vid.duration);
-    //could be used to operate play/pause in one button
-    console.log(vid.paused);
-    
+loadDemo();
 
-    vid.play();
-});
+//fetching data may take a while
+function successCallback(position)
+{
+    console.log(position);
+}
 
-document.getElementById("pause").addEventListener("click", function () {
-    vid.pause();
-});
+function errorCallback(error) {
+    console.log(error);
+    switch(error.code){
+        case 0:
+            updateStatus("There was an error while retrieving your location: " + error.message);
+            break;
+        case 1:
+            updateStatus("The user prevented this page from retrieving a location.");
+            break;
+        case 2:
+            updateStatus("The browser was unable to determine your location: " + error.message);
+            break;
+        case 3:
+            updateStatus("The browser timed out before retrieving the location.");
+            break;
+    }
+}
+
+function updateStatus(status) {
+    console.log(status);
+}
